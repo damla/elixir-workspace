8dc896eb6300767bbb38d190333c6bdf75d66f56
Damla Koksal <me@damlakoksal.com>
2023-07-16 20:29:07 +0300

    feat: add double_each function

--- math-recursive.exs
+++ math-recursive.exs
@@ -80,4 +80,12 @@ defmodule MyApp.Math.Recursive do
   def sum_list([], accumulator) do
     accumulator
   end
+
+  def double_each([head | tail]) do
+    [head * 2 | double_each(tail)]
+  end
+
+  def double_each([]) do
+    []
+  end
 end
